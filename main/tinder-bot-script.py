224165b45792dd97cee3833eeb57c8b057ef8609
Alberto <albertiae.digital@gmail.com>
2022-10-11 19:54:28 -0400

    Redirected the script

--- main/tinder-bot-script.py
+++ main/tinder-bot-script.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.chrome.options import Options
 
 # Download ChromeDriver at: https://chromedriver.chromium.org/downloads
+
+# Remember you need to rum Google Chrome on debugging mode
 
 path = # 'insert here the path of chromedriver in your computer'
 service = Service(executable_path=path)
 web = 'https://www.tinder.com/'
-driver = webdriver.Chrome(service=service)
+
+opt = Options()
+opt.ad_experimental_option('debuggerAddress', 'localhost:#')
+driver = webdriver.Chrome(service=service, options=opt)
