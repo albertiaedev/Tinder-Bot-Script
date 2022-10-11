b770709921fbded50cefc71d9500a60a046843a6
Alberto <albertiae.digital@gmail.com>
2022-10-11 19:29:41 -0400

    Specified the path, service, web and driver for our script

--- main/tinder-bot-script.py
+++ main/tinder-bot-script.py
@@ -1,2 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+
+
+# Download ChromeDriver at: https://chromedriver.chromium.org/downloads
+
+path = # 'insert here the path of chromedriver in your computer'
+service = Service(executable_path=path)
+web = 'https://www.tinder.com/'
+driver = webdriver.Chrome(service=service)
