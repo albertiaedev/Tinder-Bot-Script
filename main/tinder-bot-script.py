d38a62f25798476e9fb951f311ec0ec6c00f741b
Alberto <albertiae.digital@gmail.com>
2022-10-11 19:56:59 -0400

    Fixed the typos in the script

--- main/tinder-bot-script.py
+++ main/tinder-bot-script.py
@@ -4,12 +4,12 @@ from selenium.webdriver.chrome.options import Options
 
 # Download ChromeDriver at: https://chromedriver.chromium.org/downloads
 
-# Remember you need to rum Google Chrome on debugging mode
+# Remember you need to run Google Chrome on debugging mode
 
 path = # 'insert here the path of chromedriver in your computer'
 service = Service(executable_path=path)
 web = 'https://www.tinder.com/'
 
 opt = Options()
-opt.ad_experimental_option('debuggerAddress', 'localhost:#')
+opt.add_experimental_option('debuggerAddress', 'localhost:#')
 driver = webdriver.Chrome(service=service, options=opt)
