65c8d70c18e75355d80cdc0c619e9c495bc48735
Alberto <albertiae.digital@gmail.com>
2022-10-11 21:16:31 -0400

    Created a variable to indicate the total swipes we'll do when running the script and inserted the code in a for loop

--- main/tinder-bot-script.py
+++ main/tinder-bot-script.py
@@ -15,26 +15,31 @@ opt = Options()
 opt.add_experimental_option('debuggerAddress', 'localhost:#')
 driver = webdriver.Chrome(service=service, options=opt)
 
+total_likes = 30
+
 driver.get(web)
 
-# automate 'swipe right'
-like = driver.find_element(by='xpath', value='//button//span[text()="Like"]')
-driver.execute_script("argument[0].click();", like)
-time.sleep(2)
+for i in range(total_likes):
+    try:
+        # automate 'swipe right'
+        like = driver.find_element(by='xpath', value='//button//span[text()="Like"]')
+        driver.execute_script("argument[0].click();", like)
+        time.sleep(2)
 
-# automate a match message
-match = driver.find_element(by='xpath', value='//textarea[@placeholder="Say something nice!"]')
-#you may need to change the placeholder to your local language
-hook = "They say love is important, but I think YOU are more important" #yes, it's cheesy. Don't blame me lol
-match.send_keys(hook)
+        # automate a match message
+        match = driver.find_element(by='xpath', value='//textarea[@placeholder="Say something nice!"]')
+        #you may need to change the placeholder to your local language
+        hook = "They say love is important, but I think YOU are more important" #yes, it's cheesy. Don't blame me lol
+        match.send_keys(hook)
 
-send = driver.find_element(by='xpath', value='//button/span[text()="Send"]')
-send.click()
-time.sleep(1)
+        send = driver.find_element(by='xpath', value='//button/span[text()="Send"]')
+        send.click()
+        time.sleep(1)
 
-close = driver.find_element(by='xpath', value='//button[@title="Back to Tinder"]')
-close.click()
+        close = driver.find_element(by='xpath', value='//button[@title="Back to Tinder"]')
+        close.click()
 
-# get rid of tinder ads
-adblock = driver.find_element(by='xpath', value='//button/span[text()="Maybe later"] | //button/span[text()="Not interested"] | //button/span[text()="No thanks"]')
-adblock.click()
+        # get rid of tinder ads
+        adblock = driver.find_element(by='xpath', value='//button/span[text()="Maybe later"] | //button/span[text()="Not interested"] | //button/span[text()="No thanks"]')
+        adblock.click()
+    except:
